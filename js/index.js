b5575b16cf7b97b0957cdd85e87fc2fdd4edc859
Esteark <eardila1409@gmail.com>
2023-08-20 22:38:14 -0500

    Add changes second button

--- js/index.js
+++ js/index.js
@@ -1,10 +1,27 @@
 const btnQuizMain = document.getElementById("btnQuizMain");
 const btnOther = document.getElementById("btnOther");
 
+const Notify = (mensaje, tipo) => {
+  Toastify({
+    text: `${mensaje}`,
+    duration: 3000,
+    close: true,
+    gravity: "top", // `top` or `bottom`
+    position: "left", // `left`, `center` or `right`
+    stopOnFocus: true, // Prevents dismissing of toast on hover
+    style: {
+      background: `${
+        tipo !== "" ? (tipo == "correcto" ? "#8c7ae6" : "#c23616") : "#c23616"
+      }`,
+    },
+    onClick: function () {}, // Callback after click
+  }).showToast();
+};
+
 btnQuizMain.addEventListener("click", () => {
   window.location.href = "./views/quizMain.html";
 });
 
 btnOther.addEventListener("click", () => {
-  window.location.href = "./views/quizMainPhp.html";
+  Notify("Lo sentimos peno no tenemos conexión a la base de datos 😞", "");
 });
